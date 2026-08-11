b14001b83cb9461193fb0ae09dc0470924eeef47
aryanisbest66-alt <aryanisbest66@gmail.com>
2026-08-11 10:23:36 +0530

    Add files via upload
    ..

---
+++ sum series.py
@@ -0,0 +1,13 @@
+x=int(input("enter the value of x:"))
+n=int(input("enter value of n:"))
+var= 0
+for i in range(0,n+1):
+      var= var+(x**i)
+      print("sum of the series is",var)
+      
+      
+
+
+                
+                
+            
